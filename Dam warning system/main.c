fb7603626052c6d4bb1dcf22299e2b4d39deb713
Naresh Kalyanasundaram <naresh.kalyanasundaram@gmail.com>
2020-06-10 16:12:52 +0530

    Function for Evaluation instructions.

--- Dam warning system/main.c
+++ Dam warning system/main.c
@@ -1,3 +1,5 @@
+#define density 2400
+#define acceleration 10
 //Creating a structure variable to store several dam details.
 typedef struct dam_records
 {
@@ -9,6 +11,53 @@ typedef struct dam_records
     double river_length;
     double rainfall;
 }d;
+
+//This function is used to convey the suggested instructions to be followed according to the given parameters.
+void evacuation_instructions(int num)
+{
+    //This message is when the flag is green (i.e.)when there is no danger.
+    char dam1[100]={"\n\n\t The dam may remain closed as there is no threat at all."};
+    char resc1[100]={"\n\t Evacuation is NOT necessary and NO volunteers will be required for the time being."};
+    //This message is when the flag is yellow (i.e.)when there is slight danger.
+    char dam2[100]={"\n\n\t Suggested measures are to EVACUATE people in a 1 KM radius surrounding the dam."};
+    char resc2[200]={"\n\t Once people have been evacuated, the dam is to be opened to prevent sturctural damage to the dam.\n\t "
+                     "Around 10-20 volunteers will be required to aid in cleanup and for help in evacuation."};
+    //This message is when the flag is orange (i.e.)when the is significant danger.
+    char dam3[100]={"\n\n\t It is HIGHLY advised that people in a 5 to 6 KM radius be EVACUATED IMMEDIATELY"};
+    char resc3[300]={"\n\t The dam is to be opened immediately after the evacuation process has been completed as an alarming amount of stress is being put on the dam.\n\t "
+                     "Disaster Management teams to be alerted and kept on standby incase of emergencies.\n\t"
+                     "Around 40 to 50 volunteers are to be requested to aid in the evacuation."};
+    //This message is when the flag is red (i.e.)when there is high threat of disaster.
+    char dam4[400]={"\n\n\t IMMINENT DANGER! EXCESS AMOUNTS OF STRESS IS BEING PUT ON THE DAM.\n\t"
+                    " PEOPLE IN A 10 KM TO BE EVACUATED IMMEDIATELY."};
+    char resc4[300]={"\n\t HIGHLY ADVISED THAT THE DAM BE OPENED ASAP TO PREVENT BREAKING OF THE DAM.\n\t "
+                     "DISASTER MANAGEMENTS TEAMS TO BE KEPT ON CALL READY TO RESPOND.\n\t"
+                     "Several hundred, more than 200 volunteers are to be resqusted and kept on standby to aid in the rescue and evacuation."};
+    char inst1[100]={"\nINSTRUCTIONS\n"};
+    char inst2[100]={"\t\t\tEVACUATION AND OPENING OF DAM:"};
+    file_print(inst1,inst2);
+    //Printing the appropriate details from the above based on the input given.
+    if(num == 1)
+    {
+        file_print(dam1,resc1);
+    }
+    else if(num == 2)
+    {
+        file_print(dam2,resc2);
+    }
+    else if(num == 3)
+    {
+        file_print(dam3,resc3);
+    }
+    else if(num == 4)
+    {
+        file_print(dam4,resc4);
+    }
+    else
+    {
+	printf("\n");
+    }
+}
 
 //Storing the entered details of the dam in a file for future reference.
 void file_print_struct(d dam)
