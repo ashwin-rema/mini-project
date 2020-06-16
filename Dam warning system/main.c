fc2263da5dd9103c31b05027ec1bff11b1924d37
ashwin-rema <65447173+ashwin-rema@users.noreply.github.com>
2020-06-16 07:12:25 -0700

    Replacing main function and other functions

--- Dam warning system/main.c
+++ Dam warning system/main.c
@@ -1,5 +1,9 @@
-#define density 2400
-#define acceleration 10
+//C program to create an early warning system
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define g 9.8
 
 //Creating a structure variable to store several dam details.
 typedef struct flood_system
@@ -17,6 +21,7 @@ typedef struct flood_system
 
 int capacity(m dam);
 double calculate(m dam, double shut_height);
+double square(double number);
 int decision(m dam, double flow_out);
 void cond(int check, int flag, int y);
 void file_print_struct(m dam, int n);
@@ -94,11 +99,31 @@ int capacity(m dam)
     }
 }
 
-//Function to calculate the outward flow of water in the dam
+double square(double number)
+{
+    
+    double temp, squrt;
 
+    // store the half of the given number e.g from 256 => 128
+    sqrt = number / 2;
+    temp = 0;
+
+
+    // Iterate until sqrt is different of temp, that is updated on the loop
+    while(squrt != temp){
+        // initially 0, is updated with the initial value of 128
+        // (on second iteration = 65)
+        // and so on
+        temp = squrt;
+	    
+     return squrt;
+
+        // Then, replace values (256 / 128
+//Function to calculate the outward flow of water in the dam
+}
 double calculate(m dam, double shut_height)
 {
-    double velocity = sqrt((2*g*shut_height));
+    double velocity = square((2*g*shut_height));
     double area = dam.width * shut_height;
     return (area * velocity);
 }
@@ -147,66 +172,60 @@ void file_print_struct(m dam, int n)
     fclose(fptr);                            
 }
 
-//Warning System
-void warning_system(d dam)
+
+//Main function
+
+int main()
 {
+    FILE *ftr;
+
+    //Creating a file called 'records'
+    ftr=fopen("records.txt","w");
+
+    //Closing the file
+    fclose(ftr);
+
+    printf("\t\t\t\t\tEARLY WARNING FLOOD SYSTEM\n\n");
+    int n;
     int num;
-	
-    //Singal messages
-	
-    char signal_1[10] = {" GREEN"};
-    char signal_2[10] = {" ORANGE!"};
-    char signal_3[10] = {" YELLOW!!"};
-    char signal_4[10] = {" RED!!!!"};
-    char signal_5[30] = {" WRONG INFORMATION GIVEN"};
-    char msg_1[30] = {" No need to panic"};
-    char msg_2[50] = {" Take extreme precaution and steps necessary"};
-    char msg_3[50] = {" Prepare for evacuation immediately"};
-    char msg_4[70] = {" Alert!! Evacuate immediately"};
-    char msg_5[70] = {" KINDLY ENTER CORRECT INFORMATION TO PROCESS THE WARNING\n\n"};
-	
-    double s_height , md_height , d_height;
-    s_height = (0.7 * dam.dam_height);
-    md_height = (0.85 * dam.dam_height);
-    d_height = (0.9 * dam.dam_height);
-	
-    if(dam.water_level <= (s_height))
+    printf("\nEnter the number of dams in the system that are being monitored :");
+
+    scanf("%d", &n);
+    m dam[n];
+    for(int i=0; i<n; i++)
     {
-	file_print(signal_1, msg_1);
-	num = 1;
-	evacuation_instructions(num);
-	breaking_force(dam);
-    }
-	
-    else if((dam.water_level > s_height) && (dam.water_level <= md_height))
-    {
-	file_print(signal_2, msg_2);
-	num = 2;
-	evacuation_instructions(num);
-	breaking_force(dam);
-    }
-	
-    else if((dam.water_level > md_height) && (dam.water_level <= d_height))
-    {
-	file_print(signal_3, msg_3);
-	num = 3;
-	evacuation_instructions(num);
-	breaking_force(dam);
-    }   
-	
-    else if((dam.water_level > d_height) && (dam.water_level <= dam.dam_height))
-    {
-	file_print(signal_4, msg_4);
-	num = 4;
-	evacuation_instructions(num);
-	breaking_force(dam);
-    }
-	
-    else 
-    {
-        file_print(signal_5, msg_5);
-    }
-}
+        printf("\nEnter the name of the dam :");
+        scanf("%s", dam[i].name);
+
+        printf("\nEnter the threshold capacity of the dam in SI units :");
+        scanf("%lf", &dam[i].threshold);
+
+        printf("\nEnter the lateral area of the contact surface in SI units :");
+        scanf("%lf", &dam[i].lat_area);
+
+        printf("\nEnter the inward flow in the dam in SI units :");
+        scanf("%lf", &dam[i].flow_in);
+
+        printf("\nEnter the width of the dam shutter in SI units :");
+        scanf("%lf", &dam[i].width);
+
+        printf("\nEnter the critical height of the dam in SI units :");
+        scanf("%lf", &dam[i].init_height);
+
+        printf("\nEnter the water level in the dam in SI units :");
+        scanf("%lf", &dam[i].water_level);
+
+        //Saving the details of the dam into a file
+        file_print_struct(dam[i], n);
+        double flow_out = 0.00, shut_height = 0.00;
+        int check, flag, y = 0;
+
+        //Checking the criticality of the dam
+
+        check = capacity(dam[i]);
+        flag = decision(dam[i], flow_out);
+        cond(check, flag, y);
+        y = y + 1;
 
 
 
