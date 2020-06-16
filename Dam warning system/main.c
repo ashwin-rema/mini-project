469167165f06d9a0696490d6a2adc824f4486b9f
Naresh2002k <66559080+Naresh2002k@users.noreply.github.com>
2020-06-16 19:00:16 +0530

    Eval function has been changed to cond function.
    Several other minor changed too.

--- Dam warning system/main.c
+++ Dam warning system/main.c
@@ -13,58 +13,69 @@ typedef struct flood_system
     double init_height;
 }m;
 
+//Functions are declared
+
 int capacity(m dam);
 double calculate(m dam, double shut_height);
 int decision(m dam, double flow_out);
 void cond(int check, int flag, int y);
 void file_print_struct(m dam, int n);
 
+//Function to check whether the dam and its surrounding area is safe from flooding
 
-//This function is used to convey the suggested instructions to be followed according to the given parameters.
-void evacuation_instructions(int num)
+void cond(int check, int flag, int y)
+
 {
-    //This message is when the flag is green (i.e.)when there is no danger.
-    char dam1[100]={"\n\n\t The dam may remain closed as there is no threat at all."};
-    char resc1[100]={"\n\t Evacuation is NOT necessary and NO volunteers will be required for the time being."};
-    //This message is when the flag is yellow (i.e.)when there is slight danger.
-    char dam2[100]={"\n\n\t Suggested measures are to EVACUATE people in a 1 KM radius surrounding the dam."};
-    char resc2[200]={"\n\t Once people have been evacuated, the dam is to be opened to prevent sturctural damage to the dam.\n\t "
-                     "Around 10-20 volunteers will be required to aid in cleanup and for help in evacuation."};
-    //This message is when the flag is orange (i.e.)when the is significant danger.
-    char dam3[100]={"\n\n\t It is HIGHLY advised that people in a 5 to 6 KM radius be EVACUATED IMMEDIATELY"};
-    char resc3[300]={"\n\t The dam is to be opened immediately after the evacuation process has been completed as an alarming amount of stress is being put on the dam.\n\t "
-                     "Disaster Management teams to be alerted and kept on standby incase of emergencies.\n\t"
-                     "Around 40 to 50 volunteers are to be requested to aid in the evacuation."};
-    //This message is when the flag is red (i.e.)when there is high threat of disaster.
-    char dam4[400]={"\n\n\t IMMINENT DANGER! EXCESS AMOUNTS OF STRESS IS BEING PUT ON THE DAM.\n\t"
-                    " PEOPLE IN A 10 KM TO BE EVACUATED IMMEDIATELY."};
-    char resc4[300]={"\n\t HIGHLY ADVISED THAT THE DAM BE OPENED ASAP TO PREVENT BREAKING OF THE DAM.\n\t "
-                     "DISASTER MANAGEMENTS TEAMS TO BE KEPT ON CALL READY TO RESPOND.\n\t"
-                     "Several hundred, more than 200 volunteers are to be resqusted and kept on standby to aid in the rescue and evacuation."};
-    char inst1[100]={"\nINSTRUCTIONS\n"};
-    char inst2[100]={"\t\t\tEVACUATION AND OPENING OF DAM:"};
-    file_print(inst1,inst2);
-    //Printing the appropriate details from the above based on the input given.
-    if(num == 1)
-    {
-        file_print(dam1,resc1);
-    }
-    else if(num == 2)
-    {
-        file_print(dam2,resc2);
-    }
-    else if(num == 3)
-    {
-        file_print(dam3,resc3);
-    }
-    else if(num == 4)
-    {
-        file_print(dam4,resc4);
-    }
-    else
-    {
-	printf("\n");
-    }
+    int chfl = check + flag; 
+    if((chfl)==2)
+    {
+        FILE *file;
+        file = fopen("records.txt", "a");
+	    
+        printf("\nAt time t= %d hours \nSignal = RED", y);
+        printf("\n\nAlert!!The dam and its surrounding areas are on the verge of flooding."
+               "\nSo kindly evacuate the people from the surrounding areas and move them them to higher grounds until futher adviced.");
+	    
+        fprintf(file, "\nAt time t=%d hours signal=RED", y);
+        fprintf(file, "\nAlert!!The dam and its surrounding areas are on the verge of flooding."
+                "\nSo kindly evacuate the people from the surrounding areas and move them them to higher grounds until futher adviced.");
+	    
+        fclose(file);
+    }
+	
+    else if((chfl)==1)
+    {
+        FILE *file;
+        file = fopen("records.txt", "a");
+	    
+        printf("\nAt time t= %d hours \nSignal = ORANGE", y);
+        printf("\n\nThe dam and its surrounding areas are at high risk of flooding."
+               "\nSo kindly take the necessary steps required");
+	    
+        fprintf(file, "\nAt time t = %d hours \nSignal = ORANGE", y);
+        fprintf(file, "\n\nThe dam and its surrounding areas are at high risk of flooding."
+                "\nSo kindly take the necessary steps required");
+	    
+        fclose(file);
+    }
+	
+    else if((chfl)==0)
+    {
+        FILE *file;
+        file = fopen("records.txt", "a");
+	    
+        printf("\nAt time t= %d hours \nSignal = GREEN", y);
+        printf("\n\nThe dam and its surrounding area is safe from flooding now and no need to evacuate the people from neighbouring areas."
+               "\nNo need to panic.");
+	    
+        fprintf(file, "\nAt time t = %d hours \nSignal = GREEN", y);
+        fprintf(file, "\n\nThe dam and its surrounding area is safe from flooding now and no need to evacuate the people from neighbouring areas"
+        "\nNo need to panic");
+	    
+        fclose(file);
+	    
+        exit(0);
+    }
 }
 
 //Function to check if the dam capacity is below the threshold capacity
@@ -72,10 +83,15 @@ void evacuation_instructions(int num)
 int capacity(m dam)
 {
     double cap = dam.lat_area*dam.water_level;
+	
     if(cap>dam.threshold)
+    {	    
         return 1;
+    }
     else
+    {
         return 0;
+    }
 }
 
 //Function to calculate the outward flow of water in the dam
@@ -92,10 +108,16 @@ double calculate(m dam, double shut_height)
 int decision(m dam, double flow_out)
 {
     double dh = (dam.flow_in - flow_out)/dam.lat_area;
+	
     if(dh > dam.init_height)
+    {	    
         return 1;
+    }
+	
     else
+    {
         return 0;
+    }
 }
 
 //Function to store the entered details of the dam in a file for future reference
@@ -105,13 +127,14 @@ void file_print_struct(m dam, int n)
     FILE *fptr;
 
     //Opening the file called 'records'
-    fptr = fopen("records.txt","a");            
+    fptr = fopen("records.txt","a");   
+	
+    //Printing all necessary information in the "records" file
 
     fprintf(fptr, "\t\t\t\t\t\tReport\n\n");
     fprintf(fptr, "\t\t\t\t\tEarly Warning Flood System\n\n\n");
 
     fprintf(fptr, "\n\nThe number of dams in the system that are being monitored : %d", n);
-
     fprintf(fptr, "\n\nThe name of the dam : %s", dam.name);
     fprintf(fptr, "\n\nThe threshold capacity of the dam in SI units : %lf", dam.threshold);
     fprintf(fptr, "\n\nThe lateral area of the contact surface in SI units : %lf", dam.lat_area);
@@ -128,7 +151,9 @@ void file_print_struct(m dam, int n)
 void warning_system(d dam)
 {
     int num;
+	
     //Singal messages
+	
     char signal_1[10] = {" GREEN"};
     char signal_2[10] = {" ORANGE!"};
     char signal_3[10] = {" YELLOW!!"};
@@ -139,34 +164,44 @@ void warning_system(d dam)
     char msg_3[50] = {" Prepare for evacuation immediately"};
     char msg_4[70] = {" Alert!! Evacuate immediately"};
     char msg_5[70] = {" KINDLY ENTER CORRECT INFORMATION TO PROCESS THE WARNING\n\n"};
-    if(dam.water_level <= (0.7 * dam.dam_height))
+	
+    double s_height , md_height , d_height;
+    s_height = (0.7 * dam.dam_height);
+    md_height = (0.85 * dam.dam_height);
+    d_height = (0.9 * dam.dam_height);
+	
+    if(dam.water_level <= (s_height))
     {
 	file_print(signal_1, msg_1);
 	num = 1;
 	evacuation_instructions(num);
 	breaking_force(dam);
     }
-    else if((dam.water_level > (0.7 * dam.dam_height)) && (dam.water_level <= (0.85 * dam.dam_height)))
+	
+    else if((dam.water_level > s_height) && (dam.water_level <= md_height))
     {
 	file_print(signal_2, msg_2);
 	num = 2;
 	evacuation_instructions(num);
 	breaking_force(dam);
     }
-    else if((dam.water_level > (0.85 * dam.dam_height)) && (dam.water_level <= (0.9 * dam.dam_height)))
+	
+    else if((dam.water_level > md_height) && (dam.water_level <= d_height))
     {
 	file_print(signal_3, msg_3);
 	num = 3;
 	evacuation_instructions(num);
 	breaking_force(dam);
     }   
-    else if((dam.water_level > (0.9 * dam.dam_height)) && (dam.water_level <= dam.dam_height))
+	
+    else if((dam.water_level > d_height) && (dam.water_level <= dam.dam_height))
     {
 	file_print(signal_4, msg_4);
 	num = 4;
 	evacuation_instructions(num);
 	breaking_force(dam);
     }
+	
     else 
     {
         file_print(signal_5, msg_5);
